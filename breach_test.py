a4c9a869511e2ba2dc05a6f21adeab72f9de1b22
CelestialLelouch <166859858+CelestialLelouch@users.noreply.github.com>
2025-02-19 09:59:28 +0200

    Reemoved some files that don't need to be here

--- breach_test.py
+++ breach_test.py
@@ -6,14 +6,15 @@ def check_password(password):
     sha1_password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
     prefix = sha1_password[:5]
     suffix = sha1_password[5:]
-
-    response = requests.get(f'https://api.pwnedpasswords.com/range/{prefix}')
-    response.raise_for_status()
-    if response.status_code == 200:
-        if suffix in response.text:
-            return True
+    try:
+        response = requests.get(f'https://api.pwnedpasswords.com/range/{prefix}')
+        response.raise_for_status()
+        if response.status_code == 200:
+            if suffix in response.text:
+                return True
+            return False
+        else:
+            messagebox.showerror("CONNECTION ERROR", "Failed to connect to provider.")
+            return False
+    except:
         return False
-    else:
-        messagebox.showerror("CONNECTION ERROR", "Failed to connect to provider.")
-        return False
-    
